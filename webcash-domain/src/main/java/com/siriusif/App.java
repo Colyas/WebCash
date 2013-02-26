54c619e3a6202371c013ba822759e201b5a29cd1
colya <colya.s.o@gmail.com>
2013-02-26 22:33:29 +0200

    issue #28 add maven dependency and repository of BIRT

--- webcash-domain/src/main/java/com/siriusif/App.java
+++ webcash-domain/src/main/java/com/siriusif/App.java
@@ -2,12 +2,10 @@ package com.siriusif;
 
 /**
  * This console app present here only for quick and dirty testing
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
