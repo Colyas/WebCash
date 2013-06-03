13417e872af9659b2bcbd2afb0a2d0457b8211fb
colya <colya.s.o@gmail.com>
2013-06-03 10:23:53 +0300

    issue #50 URL read from property file

--- webcash-android/src/com/siriusif/android/MainActivity.java
+++ webcash-android/src/com/siriusif/android/MainActivity.java
@@ -1,26 +1,44 @@
 package com.siriusif.android;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-	
+
 	WebView mWebView;
+	final private String LOG_TAG = "logWebCashAndroid";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mWebView = (WebView) findViewById(R.id.webview);
 		mWebView.getSettings().setJavaScriptEnabled(true);
-		mWebView.loadUrl("http://10.0.2.2:8080/webcash/login.jsf");
+		mWebView.loadUrl(readProps());
 		mWebView.setWebViewClient(new WebCashWebViewClient());
 	}
-	
+
+	String readProps() {
+		Properties props = new Properties();
+		String path = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/MyFiles/settings.properties";
+		Log.d(LOG_TAG, "Property file read");
+		try {
+			props.load(new FileInputStream(path));
+		} catch (Exception e) {
+			Log.e(LOG_TAG, "Can't read file", e);
+		}
+		return props.getProperty("host");
+	}
 
 	private class WebCashWebViewClient extends WebViewClient {
 		@Override
@@ -29,8 +47,7 @@ public class MainActivity extends Activity {
 			return true;
 		}
 	}
-	
-	 
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
